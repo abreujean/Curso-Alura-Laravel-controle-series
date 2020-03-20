722cdec6df6021c456f761746b93e53ccf063f95
jeabreu <jeandcabreu@gmail.com>
2020-03-20 12:12:06 -0300

    Subindo aplicação

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/6016d1a0c1.js" crossorigin="anonymous"></script>
+
+    <title>Controle de Séries</title>
+</head>
+<body>
+    <div class="container">
+        <div class="jumbotron">
+            <h1>@yield('cabecalho')</h1>
+        </div>
+
+        @yield('conteudo')
+    </div>
+</body>
+</html>
